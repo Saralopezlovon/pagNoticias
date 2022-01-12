a4cfd306cdd19e54b5e083585415613bc61c7953
Sara <lopezlovonsara@gmail.com>
2022-01-12 14:01:36 +0100

    Add changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter } from 'react-router-dom'; //Para utilizar rutas
+import { HashRouter } from 'react-router-dom'; //Para utilizar rutas
 import {userContext} from './context/userContext'; //Para utilizar context
 
 export default class App extends Component {
@@ -23,13 +23,13 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <userContext.Provider value={this.state}>
             <Header/>
             <Main/>
           </userContext.Provider>      
 
-        </BrowserRouter>
+        </HashRouter>
         <Footer/>
   
   </div>
